24be5042d5f029e57024559825807d9ff8a5f462
DreamCatcher13 <gerey37ng@gmail.com>
2017-03-19 19:06:29 +0200

    Тести для Юрія

--- src/test/java/ua/edu/uzhnu/practice2/TreeTest.java
+++ src/test/java/ua/edu/uzhnu/practice2/TreeTest.java
@@ -12,4 +12,87 @@ public class TreeTest {
     public void test_SanityTest_JUnitWorks(){
         Assert.assertEquals(2, 1+1);
     }
+
+    @Test
+    public void test_Person (){
+        Assert.assertEquals ("Юрій", new SampleFamilyProvider().makeFamilyTree().getName());
+    }
+
+    @Test
+    public void test_Father(){
+        Assert.assertEquals ("Остап",new SampleFamilyProvider().makeFamilyTree().getFather().getName());
+    }
+
+    @Test
+    public void test_FirstGrandFather (){
+        Assert.assertEquals("Невідомий", new SampleFamilyProvider().makeFamilyTree().getFather().getFather().getName());
+    }
+
+    @Test
+    public void tesr_FirstGrandMother () {
+        Assert.assertEquals("Невідома", new SampleFamilyProvider().makeFamilyTree().getFather().getMother().getName());
+    }
+
+    @Test
+    public void tesr_FirstGrandGrandFather () {
+        Assert.assertEquals("Невідомий", new SampleFamilyProvider().makeFamilyTree().getFather().getFather().getFather().getName());
+    }
+
+    @Test
+    public void tesr_FirstGrandGrandMother () {
+        Assert.assertEquals("Невідома", new SampleFamilyProvider().makeFamilyTree().getFather().getFather().getMother().getName());
+    }
+
+    @Test
+    public void tesr_SecondGrandGrandFather () {
+        Assert.assertEquals("Невідомий", new SampleFamilyProvider().makeFamilyTree().getFather().getMother().getFather().getName());
+    }
+
+    @Test
+    public void tesr_SecondGrandGrandMother () {
+        Assert.assertEquals("Невідома", new SampleFamilyProvider().makeFamilyTree().getFather().getMother().getMother().getName());
+    }
+
+    @Test
+    public void tesr_Mother () {
+        Assert.assertEquals("Катя", new SampleFamilyProvider().makeFamilyTree().getMother().getName());
+    }
+
+    @Test
+    public void tesr_SecondGrandMother () {
+        Assert.assertEquals("Марина", new SampleFamilyProvider().makeFamilyTree().getMother(). getMother().getName());
+    }
+
+    @Test
+    public void tesr_SecondGrandFather () {
+        Assert.assertEquals("Іван", new SampleFamilyProvider().makeFamilyTree().getMother().getFather().getName());
+    }
+
+    @Test
+    public void tesr_ThirdGrandGrandMother () {
+        Assert.assertEquals("Невідома", new SampleFamilyProvider().makeFamilyTree().getMother().getMother().getMother().getName());
+    }
+
+    @Test
+    public void tesr_ThirdGrandGrandFather () {
+        Assert.assertEquals("Невідомий", new SampleFamilyProvider().makeFamilyTree().getMother().getMother().getFather().getName());
+    }
+
+    @Test
+    public void tesr_FourthGrandGrandMother () {
+        Assert.assertEquals("Невідома", new SampleFamilyProvider().makeFamilyTree().getMother().getFather().getMother().getName());
+    }
+
+    @Test
+    public void tesr_FourthGrandGrandFather () {
+        Assert.assertEquals("Невідомий", new SampleFamilyProvider().makeFamilyTree().getMother().getFather().getFather().getName());
+    }
+
+
+
+
+
+
+
+
 }
